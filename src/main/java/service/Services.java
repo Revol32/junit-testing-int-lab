ea611885b30b6cc7135264e18c254e11b4b093cd
Revol32 <72274775+Revol32@users.noreply.github.com>
2021-03-24 15:53:57 +0400

    init

---
+++ src/main/java/service/Services.java
@@ -0,0 +1,11 @@
+package service;
+
+public class Services {
+    public String concatenateWords (String fString, String sString) {
+        return null;
+    }
+
+    public long computeFactorial (int number) {
+        return 0;
+    }
+}
