5bedead7489392ffa314b5b6f3347740343003d0
Revol32 <72274775+Revol32@users.noreply.github.com>
2021-03-24 19:34:04 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import static service.Services.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(concatenateWords("Hello", "world!"));
+        System.out.println(computeFactorial(21));
+        System.out.println(9223372036854775807L);
     }
 }
--- src/test/java/Providers.java
+++ src/test/java/Providers.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class Providers {
+
+    public static Stream<Arguments> testConcatenateWordsNormalProvider() {
+        return Stream.of(
+                arguments("Hello", "world!", "Hello world!"),
+                arguments("Привет,", "мир!", "Привет, мир!")
+        );
+    }
+
+    public static Stream<Arguments> testConcatenateWordsWrongSymbolProvider() {
+        return Stream.of(
+                arguments("\uD83C\uDF08\uD83E\uDD84", "world!"),
+                arguments("Привет,", "ツ")
+        );
+    }
+
+    public static Stream<Arguments> testConcatenateWordsNullAndEmptyProvider() {
+        return Stream.of(
+                arguments("", "", ""),
+                arguments(null, null, null),
+                arguments(null, "world!", "world!"),
+                arguments("Hello", null, "Hello")
+        );
+    }
+
+    public static Stream<Arguments> testComputeFactorialProvider() {
+        return Stream.of(
+                arguments(0, BigInteger.valueOf(1)),
+                arguments(1, BigInteger.valueOf(1)),
+                arguments(5, BigInteger.valueOf(120)),
+                arguments(20, BigInteger.valueOf(2432902008176640000L)),
+                arguments(21, new BigInteger("51090942171709440000"))
+        );
+    }
 }
